1b454476900a3acb4c547f269b551179b262ef11
Bill Blake <billblake01@yahoo.ie>
2017-11-26 21:30:34 +0000

    Asset Management Guide

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import _ from 'lodash';
+import _ from "lodash";
+import "./style.css";
+import Cat from "./cat.jpg";
+import Data from './data.xml';
 
 function component() {
-    var element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  var element = document.createElement("div");
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  // Add the image to our existing div.
+  var myIcon = new Image();
+  myIcon.src = Cat;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
+
+  return element;
+}
+
+document.body.appendChild(component());
