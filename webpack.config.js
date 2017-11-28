b0b484a59ed3881dc6e547ac0d5f08cbdb9e768c
Bill Blake <billblake01@yahoo.ie>
2017-11-28 22:27:54 +0000

    Tree Shaking Guide

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
-const path = require('path');
+const path = require("path");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [new UglifyJSPlugin()]
 };
